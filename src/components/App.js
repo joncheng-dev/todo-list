9ddae3eedc29e22b6b766a6902c310c5b7da41cd
Jonathan Cheng <jcheng461@gmail.com>
2022-09-11 23:51:36 +0900

    start project for react todo list

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="App">
+        <div className="addTask">
+          <input />
+          <button>Add Task</button>
+        </div>
+        <div className="listTasks"></div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
